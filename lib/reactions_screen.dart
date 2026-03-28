3353abb981a2514b9de8090dac62ba1c66ec109a
Sadnan Samim <samimsadnan@gmail.com>
2026-03-28 20:05:52 +0600

    Tweaked Search Result Screen

--- lib/reactions_screen.dart
+++ lib/reactions_screen.dart
@@ -29,51 +29,51 @@ class _ReactionsScreenState extends State<ReactionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 0.6,
-        leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: const Icon(Icons.arrow_back)
-        ),
-        title: const Text(
-          'Likes',
-          style: TextStyle(
-            fontWeight: FontWeight.bold
-              ,
-            fontSize: 21,
-            color: darkText
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.6,
+          leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(Icons.arrow_back)
+          ),
+          title: const Text(
+            'Likes',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 21,
+                color: darkText
+            ),
           ),
         ),
-      ),
-      body: ListView.separated(
-          itemCount: dummyNames.length,
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          separatorBuilder: (context, index) => Divider(
-            height: 3,
-            color: Colors.grey[200],
-            indent: 6
-          ),
-          itemBuilder: (context, index) {
-            return ListTile(
-              contentPadding: const EdgeInsets.symmetric(vertical: 4),
-              leading: CircleAvatar(
-                radius: 24,
-                backgroundColor: Colors.grey[200],
-                child: Icon(Icons.person, color: Colors.grey, size: 28),
-              ),
-              title: Text(
-                dummyNames[index],
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: darkText,
+        body: ListView.separated(
+            itemCount: dummyNames.length,
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            separatorBuilder: (context, index) =>
+                Divider(
+                    height: 3,
+                    color: Colors.grey[200],
+                    indent: 58
                 ),
-              ),
-            );
-          }
-          )
+            itemBuilder: (context, index) {
+              return ListTile(
+                contentPadding: const EdgeInsets.symmetric(vertical: 4),
+                leading: CircleAvatar(
+                  radius: 24,
+                  backgroundColor: Colors.grey[200],
+                  child: Icon(Icons.person, color: Colors.grey, size: 28),
+                ),
+                title: Text(
+                  dummyNames[index],
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: darkText,
+                  ),
+                ),
+              );
+            }
+        )
     );
   }
-  
+
 }
